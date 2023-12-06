159b04dca21c8ea162ea73528ae41f165c36243e
Thiago Conceição <thiago.conceicao@oliveiratrust.com.br>
2023-12-06 11:20:37 -0300

    changes in events

--- src/eventosapp/models.py
+++ src/eventosapp/models.py
@@ -7,3 +7,14 @@ from django.db import models
 class FundoXP(models.Model):
     fundo = models.CharField(max_length=30)
     diaUtil = models.IntegerField()
+
+
+class EventosDiarios(models.Model):
+    ativo = models.CharField(max=200)
+    emissor = models.CharField(max_length=250)
+    data_base = models.DateTimeField()
+    data_liquidacao = models.DateTimeField()
+
+
+
+
